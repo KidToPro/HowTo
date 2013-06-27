736b1a9f30b9dc4fb7c3e6e4da1d068545e9475f
Angel <eric@kidtopro.com>
2013-06-27 09:02:47 -0500

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowTo::Application.config.secret_token = 'ac53b5b04b16f7bc2025c84895fb91576558241ef6812d2368812ed90a8d4fb53ed477d5605077a55885585df0e58845174d9d2417b1602adfa2e666744679e2'
